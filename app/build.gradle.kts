41f510c30646181cf257811315595faee25b127b
dest-os <destosares@gmail.com>
2026-08-05 14:17:28 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
+implementation("androidx.appcompat:appcompat:1.7.0")
 }
